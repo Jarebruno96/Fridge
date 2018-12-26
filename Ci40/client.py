079d260beb4c4247bc5bbe6de798798565768c1d
Juan Arellano <juanarellano1996@gmail.com>
2018-12-26 23:44:29 +0100

    Ci40 can update temperature limit from Thingsboard

--- Ci40/client.py
+++ Ci40/client.py
@@ -2,71 +2,113 @@ import os
 from subprocess import PIPE, Popen
 import time
 import socket
-import time 
+import threading
+import json
+import shlex
 
 
-UDP_IP = 'fe80::28e9:3285:421c:bc82' # = 0.0.0.0 u IPv4
-UDP_PORT = 5005
-UDP_IP_LOCALHOST = "::1"
-UDP_IP_CLIENT_PORT = 5006
-SCALE_LSB = 0.03125
-seconds = 3
-macs = ["B0:91:22:F6:D5:03","54:6C:0E:52:F9:8F"]
-programName = "gatttool"
-error = 0
-airIsOn = 0
-limit = 15
+def getTemperatureData():
 
-sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP_LOCALHOST,UDP_IP_CLIENT_PORT))
+	UDP_IP = 'fe80::28e9:3285:421c:bc82' # = 0.0.0.0 u IPv4
+	UDP_PORT = 5005
+	UDP_IP_LOCALHOST = "::1"
+	UDP_IP_CLIENT_PORT = 5006
+	SCALE_LSB = 0.03125
+	#macs = ["B0:91:22:F6:D5:03","54:6C:0E:52:F9:8F"]
+	macs = ["54:6C:0E:52:F9:8F","54:6C:0E:52:F9:8F"]
+	programName = "gatttool"
+	error = 0
+	airIsOn = 0
 
-while True:
-	try:
-		error = 0
-		temperatures = []
+	sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM) # UDP
+	sock.bind((UDP_IP_LOCALHOST,UDP_IP_CLIENT_PORT))
 
-		for mac in macs:
+	while True:
+		try:
+			error = 0
+			temperatures = []
 
-			os.system("gatttool -b "+ mac +" --char-write-req -a 0x0027 -n 01")
+			for mac in macs:
 
-			time.sleep(1);
+				os.system("gatttool -b "+ mac +" --char-write-req -a 0x0027 -n 01")
 
-			programArgs = ["-b", mac, "--char-read", "-a", "0x0024"]
-			command = [programName]
-			command.extend(programArgs)
+				time.sleep(1);
 
-			output = Popen(command, stdout = PIPE).stdout.read().decode()
-			split = output.split(" ")
-			split = split[4:6]
-			split.reverse()
-			temp1 = ''.join(split)
+				programArgs = ["-b", mac, "--char-read", "-a", "0x0024"]
+				command = [programName]
+				command.extend(programArgs)
 
-			rawAmbTemp = int(temp1,16)
-			it = int(rawAmbTemp >> 2)
-			t = float(it * SCALE_LSB)
-			temperatures.append(t)
-			time.sleep(1);
+				output = Popen(command, stdout = PIPE).stdout.read().decode()
+				split = output.split(" ")
+				split = split[4:6]
+				split.reverse()
+				temp1 = ''.join(split)
 
-		temperatures.append(15.2)
+				rawAmbTemp = int(temp1,16)
+				it = int(rawAmbTemp >> 2)
+				t = float(it * SCALE_LSB)
+				temperatures.append(t)
+				time.sleep(1);
 
-		if temperatures[0]==0 or temperatures[1]==0:
-			error = 1
-			print("Can not read temperature from "+str(mac))
+			if temperatures[0]==0 or temperatures[1]==0:
+				error = 1
+				print("Can not read temperature from "+str(mac))
 
-		if error == 0:
+			if error == 0:
 
-			os.system('mosquitto_pub -d -h "demo.thingsboard.io" -t "v1/devices/me/telemetry" -u "tyi178C9h6rhjT4YotCW" -m "{"temperature":'+str(temp1)+'}"') #Sensor 1
-			os.system('mosquitto_pub -d -h "demo.thingsboard.io" -t "v1/devices/me/telemetry" -u "QZNPiTFGCVaYzt8c01yL" -m "{"temperature":'+str(temp2)+'}"') #Sensor 2
+				os.system('mosquitto_pub -d -h "demo.thingsboard.io" -t "v1/devices/me/telemetry" -u "tyi178C9h6rhjT4YotCW" -m "{"temperature":'+str(temperatures[0])+'}"') #Sensor 1
+				os.system('mosquitto_pub -d -h "demo.thingsboard.io" -t "v1/devices/me/telemetry" -u "QZNPiTFGCVaYzt8c01yL" -m "{"temperature":'+str(temperatures[1])+'}"') #Sensor 2
 
-			if temperatures[0] < limit or temperatures[1] < limit:
-				if airIsOn == 0:
-					sock.sendto(b"EA",(UDP_IP,UDP_PORT))
-					airIsOn = 1
+				if temperatures[0] < temperatureLimit or temperatures[1] < temperatureLimit:
+					if airIsOn == 0:
+						sock.sendto(b"EA",(UDP_IP,UDP_PORT))
+						airIsOn = 1
 
-			if temperatures[0] > limit and temperatures[1] > limit:
-				if airIsOn == 1:
-					sock.sendto(b"AA", (UDP_IP, UDP_PORT))
-					airIsOn = 0
+				if temperatures[0] > temperatureLimit and temperatures[1] > temperatureLimit:
+					if airIsOn == 1:
+						sock.sendto(b"AA", (UDP_IP, UDP_PORT))
+						airIsOn = 0
 
-	except Exception:
-		print("Can not read temperature data from "+str(mac))
+		except Exception:
+			print("Can not read temperature data from "+str(mac))
+
+
+def mosquittoSub(command):
+
+	global temperatureLimit
+
+	process = Popen(shlex.split(command), stdout=PIPE)
+	while True:
+		output = process.stdout.readline()
+		if output == '' and process.poll() is not None:
+			break
+		if output:
+			#print (output.strip().decode())
+			try:
+				msg = json.loads(output.strip().decode())
+				if "TemperatureLimit" in msg:
+					temperatureLimit = float(msg["TemperatureLimit"])
+					print("Nuevo Limite de "+str(temperatureLimit))
+			except Exception:
+				pass
+	rc = process.p
+	return rc
+
+
+def getSettings():
+
+	mosquittoSub('mosquitto_sub -d -h "demo.thingsboard.io" -t "v1/devices/me/attributes" -u "D0b1KhmyqeKaZMkiGdkC"')
+
+
+def main():
+
+	temperatureLimit = 30
+
+	t1 = threading.Thread(target=getTemperatureData, args=[])
+	t2 = threading.Thread(target=getSettings, args=[])
+	t1.start()
+	t2.start()
+
+
+if __name__ == "__main__":
+	main()
